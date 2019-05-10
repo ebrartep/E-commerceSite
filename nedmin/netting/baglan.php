fb8c3df19e0e21abd9d84b4007bb851731b46820
ebrartep <47458587+ebrartep@users.noreply.github.com>
2019-05-10 22:12:43 +0300

    Update baglan.php

--- nedmin/netting/baglan.php
+++ nedmin/netting/baglan.php
@@ -2,9 +2,8 @@
 
 try{
 
-//$db=new PDO("mysql:host=localhost;dbname=kucukgez_eticaret;charset=utf8",'kucukgez_root','Titanik555');
 
-$db=new PDO("mysql:host=localhost;dbname=eticaret;charset=utf8",'root','12345678');
+$db=new PDO("mysql:host=localhost;dbname=eticaret;charset=utf8",'username','password');
 
 //echo "veritabanı bağlantısı basarılı";
 }
